7853670334a597c3e81cc7ecbf12e7a08c8bf3bf
ClaudiaHazard <claudia.hazard.14@sansano.usm.cl>
2020-11-23 17:48:15 -0300

    mensajeria creada en los distintos .go

---
+++ DataNode3/DataNode3.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	connection "github.com/ClaudiaHazard/Tarea2/Connection"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var wg sync.WaitGroup
+
+//IP local 10.6.40.164
+const (
+	//ipport = "10.6.40.161:50051"
+	ipport = ":50051"
+)
+
+//IniciaCliente inicia conexion cliente
+func IniciaCliente() *grpc.ClientConn {
+	var conn *grpc.ClientConn
+
+	conn, err := grpc.Dial(ipport, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %s", err)
+	}
+	defer conn.Close()
+	return conn
+}
+
+//EsperaChunks espera chunks provenientes de cliente y otro datanode.
+func EsperaChunks(conn *grpc.ClientConn) *connection.Chunk {
+	c := connection.NewMensajeriaServiceClient(conn)
+	ctx := context.Background()
+
+	response, err := c.RecibeChunks(ctx, &connection.Message{Message: "En espera"})
+
+	if err != nil {
+		log.Fatalf("Error al llamar RecibeChunks: %s", err)
+	}
+
+	return response
+}
+
+//EnviaPropuestaDistribuida envia propuesta distribuida
+func EnviaPropuestaDistribuida(conn *grpc.ClientConn) *connection.Message {
+	c := connection.NewMensajeriaServiceClient(conn)
+	ctx := context.Background()
+
+	response, err := c.EnviaPropuesta(ctx, &connection.Propuesta{})
+
+	if err != nil {
+		log.Fatalf("Error al llamar EnviaPropuesta: %s", err)
+	}
+
+	return response
+}
+
+//EnviaPropuestaCentralizada envia propuesta centralizada
+func EnviaPropuestaCentralizada(conn *grpc.ClientConn) *connection.Message {
+	c := connection.NewMensajeriaServiceClient(conn)
+	ctx := context.Background()
+
+	response, err := c.EnviaPropuesta(ctx, &connection.Propuesta{})
+
+	if err != nil {
+		log.Fatalf("Error al llamar EnviaPropuesta: %s", err)
+	}
+
+	return response
+}
+
+//EnviaChunks envia chunks con la distribucion que fue aceptada previamente
+func EnviaChunks(conn *grpc.ClientConn) *connection.Message {
+	c := connection.NewMensajeriaServiceClient(conn)
+	ctx := context.Background()
+
+	response, err := c.DistribuyeChunks(ctx, &connection.Chunk{})
+
+	if err != nil {
+		log.Fatalf("Error al llamar EnviaPropuesta: %s", err)
+	}
+
+	return response
+}
+
+func main() {
+	var conn *grpc.ClientConn
+
+	//Se crea la conexion con el servidor Logistica
+	conn, err := grpc.Dial(ipport, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("No se pudo conectar: %s", err)
+	}
+
+	go EsperaChunks(conn)
+
+}
---
+++ NameNode/NameNode.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"sync"
+
+	connection "github.com/ClaudiaHazard/Tarea2/Connection"
+	"google.golang.org/grpc"
+)
+
+var wg sync.WaitGroup
+
+//Server datos
+type Server struct {
+	id int
+}
+
+//IP local 10.6.40.161
+const (
+	ipport = "10.6.40.162:50051"
+	//ipport = ":50051"
+)
+
+//CargaArchivo carga archivo a un datanode
+func (s *Server) CargaArchivo(ctx context.Context, in *connection.Propuesta) (*connection.Message, error) {
+
+	return &connection.Message{Message: "Ok"}, nil
+}
+
+//ConsultaUbicacionArchivo consulta ubicacion al namenode de los chunks en los datanodes
+func (s *Server) ConsultaUbicacionArchivo(ctx context.Context, in *connection.Propuesta) (*connection.Message, error) {
+
+	return &connection.Message{Message: "Ok"}, nil
+}
+
+//DescargaChunk descarga un chunk de alguno de los datanodes
+func (s *Server) DescargaChunk(ctx context.Context, in *connection.Propuesta) (*connection.Message, error) {
+
+	return &connection.Message{Message: "Ok"}, nil
+}
+
+//RecibeChunks Recibe propuesta de un namenode
+func (s *Server) RecibeChunks(ctx context.Context, in *connection.Propuesta) (*connection.Message, error) {
+
+	return &connection.Message{Message: "Ok"}, nil
+}
+
+//EnviaPropuesta en el caso de namenode recibe propuesta de distribucion rechaza o acepta y guarda dicha distribucion, en el caso que venga aceptada solo la guarda.
+func (s *Server) EnviaPropuesta(ctx context.Context, in *connection.Propuesta) (*connection.Message, error) {
+
+	return &connection.Message{Message: "Ok"}, nil
+}
+
+//DistribuyeChunks distribuye los chunks segun la propuesta aceptada
+func (s *Server) DistribuyeChunks(ctx context.Context, in *connection.Propuesta) (*connection.Message, error) {
+
+	return &connection.Message{Message: "Ok"}, nil
+}
+
+func main() {
+	// Escucha las conexiones grpc
+	lis, err := net.Listen("tcp", ipport)
+
+	if err != nil {
+		log.Fatalf("Failed to listen on "+ipport+": %v", err)
+	}
+
+	s := Server{id: 1}
+
+	grpcServer := grpc.NewServer()
+
+	fmt.Println("En espera de Informacion Chunks para servidor")
+
+	//Inicia el servicio de mensajeria que contiene las funciones grpc
+	connection.RegisterMensajeriaServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC server over "+ipport+": %v", err)
+	}
+
+}
