62ede54110f6f3d06f178b6fbf18782ad1a1fd34
samuelmarinn <55009185+samuelmarinn@users.noreply.github.com>
2020-12-02 12:30:58 -0300

    Cambios en prints

--- Cliente/Uploader/Uploader.go
+++ Cliente/Uploader/Uploader.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"time"
+    "strings"
 
 	connection "github.com/ClaudiaHazard/Tarea2/Connection"
 
@@ -15,7 +16,7 @@ import (
 	"os"
 	"strconv"
 	"sync"
-	//"bufio"
+    "io/ioutil"
 )
 
 var wg sync.WaitGroup
@@ -37,16 +38,13 @@ func HaceChunk(ch []byte, pos int32, n string, t int32) *connection.Chunk {
 func CreaChunks(name string, conn1 *grpc.ClientConn, conn2 *grpc.ClientConn, conn3 *grpc.ClientConn) {
 
 	work := false
-
 	seed := 0
-
 	xcon, _ := grpc.Dial(":50052")
 	c := connection.NewMensajeriaServiceClient(xcon)
 
 	for work != true {
 		//elección de a donde enviar
 		seed = rand.Intn(3)
-
 		if seed == 0 {
 			c = connection.NewMensajeriaServiceClient(conn1)
 		}
@@ -75,28 +73,21 @@ func CreaChunks(name string, conn1 *grpc.ClientConn, conn2 *grpc.ClientConn, con
 	defer file.Close()
 	fileInfo, _ := file.Stat()
 	var fileSize int64 = fileInfo.Size()
-	const fileChunk = (1 * (1 << 20)) / 4 // 250 KB
-	// num de partes en que se dividirá
+	const fileChunk = (1 * (1 << 20)) / 4 
 	totalPartsNum := int32(math.Ceil(float64(fileSize) / float64(fileChunk)))
-	fmt.Printf("Splitting to %d pieces.\n", totalPartsNum)
-
+	fmt.Println("El archivo" + name + "Tiene " + strconv.Itoa(int(totalPartsNum)) + " partes")
 	for i := int32(0); i < totalPartsNum; i++ {
 		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
 		partBuffer := make([]byte, partSize)
 		file.Read(partBuffer)
-		ch := HaceChunk(partBuffer, i+1, name, totalPartsNum) //nombre con formato incluido
-
-		fmt.Println("Enviando chunk n° " + strconv.Itoa(int(i)) + " de " + strconv.Itoa(int(ch.NumeroPar)) + "de Libro: " + name)
-
+		ch := HaceChunk(partBuffer, i+1, name, totalPartsNum)
 		_, err := c.EnviaChunkCliente(context.Background(), ch)
 
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-
 		fmt.Println("Enviado chunk n° " + strconv.Itoa(int(i)) + " de " + strconv.Itoa(int(ch.NumeroPar)))
-
 	}
 	fmt.Println(name, " terminó de enviarse")
 	wg.Done()
@@ -108,7 +99,6 @@ func main() {
 	var connDN2 *grpc.ClientConn
 	var connDN3 *grpc.ClientConn
 
-	//Se crean conexiones
 	connDN1, err := grpc.Dial(ipportDataNode1, grpc.WithInsecure(), grpc.WithBlock())
 	connDN2, err2 := grpc.Dial(ipportDataNode2, grpc.WithInsecure(), grpc.WithBlock())
 	connDN3, err3 := grpc.Dial(ipportDataNode3, grpc.WithInsecure(), grpc.WithBlock())
@@ -124,21 +114,24 @@ func main() {
 	if err3 != nil {
 		log.Fatalf("No se pudo conectar: %s", err)
 	}
-	nas := [5]string{"2.pdf", "Emma.pdf", "Dracula.pdf", "Test.pdf", "Lab1.pdf"}
 
-	wg.Add(1)
-	go CreaChunks(nas[0], connDN1, connDN2, connDN3)
-	wg.Add(1)
-	go CreaChunks(nas[1], connDN1, connDN2, connDN3)
-	fmt.Println("Espera 10 segundos para botar un DataNode en caso de que se quiera probar")
-	time.Sleep(10000 * time.Millisecond)
-	wg.Add(1)
-	go CreaChunks(nas[2], connDN1, connDN2, connDN3)
-	wg.Add(1)
-	go CreaChunks(nas[3], connDN1, connDN2, connDN3)
-	wg.Add(1)
-	go CreaChunks(nas[4], connDN1, connDN2, connDN3)
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+	    fmt.Println("Error obteniendo archivos: ",err)
+	}
 
-	wg.Wait()
-
+	var toread []string
+	for _, f := range files {
+		if ((f.IsDir()!=true) && (strings.Contains(f.Name(),".go")!=true) && (f.Name()!="Makefile")){	
+			toread=append(toread,f.Name())
+	    }
+	}
+	for i:=0;i<len(toread);i++ {
+		wg.Add(1)
+		go CreaChunks(toread[i], connDN1, connDN2, connDN3)
+	    if (i==1){
+			fmt.Println("Espera 10 segundos para botar un DataNode en caso de que se quiera probar")
+			time.Sleep(10000 * time.Millisecond)	    	
+		}
+	}
 }
